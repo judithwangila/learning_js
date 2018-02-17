1a2f4320fc3844c9901211a2c97153fc2859a434
judith wangila <judithnwangila@gmail.com>
2018-02-17 13:24:31 +0300

    learning js

---
+++ ForLoop.js
@@ -0,0 +1,8 @@
+var daysOfTheWeek = 7;
+
+for(i=0; i<= daysOfTheWeek; i++){
+console.log(i)
+}
+
+
+
---
+++ variables.js
@@ -0,0 +1,7 @@
+var classSize = 23;
+var isWeekday = false;
+var className = "Akirachix"
+var shoeSize = 6;
+
+// This helps to print
+console.log(classSize)
